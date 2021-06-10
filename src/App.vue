227956c1d36378c661d10aaa441d9f5cefcaf882
Milchmaa <marc.welz03@gmail.com>
2021-06-10 14:51:06 +0200

    added settings and some minor changes to fetch

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <div class="app">
     <div class="main-app">
       <div class="main-app__setting">
+        <p class="main-app__setting--ip">{{ serverip }}</p>
         <button class="main-app__setting--button" @click="changeSettings"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"/></svg></button>
       </div>
       <div class="main-app__buttons">
@@ -40,6 +41,8 @@ export default {
   },
   setup() {
 
+    console.log(window.location.hostname + ":8000")
+
     const propsPlayers = ref([
       {id: 1, name: 'Minecrafter2028'}, {id: 2, name: 'Milchmaa'}, {id: 3, name: 'DragonBlood'}, {id: 4, name: 'HansWurst69'}, { id: 5, name: 'Bubii'}, 
       { id: 6, name: 'Recker23'}, { id: 7, name: 'InformatikerEFZ'}, { id: 8, name: 'Erazer'}, { id: 9, name: 'Dansent'}, { id: 10, name: 'Axo'}, { id: 11, name: 'ZeferiaFOX'},
@@ -47,6 +50,7 @@ export default {
       { id: 17, name: 'Bumeli'}, { id: 18, name: 'Joel2'}
     ])
 
+    let serverip = ref("")
     const timestamp = ref([])
     const status = ref(false)
 
@@ -56,7 +60,6 @@ export default {
     }
 
     const changeSettings = () => {
-      // openSettings.value = !openSettings.value
       document.getElementsByTagName("axa-modal")[0].setAttribute("open", true)
     }
 
@@ -89,6 +92,10 @@ export default {
       return date + " " + time
     }
 
+    window.addEventListener("keypress", function(e) {
+      console.log(String.fromCharCode(e.keyCode));
+    });
+
     function updateStatus() {
       fetch('http://192.168.1.232:8000/server/status', {
            method: "get",
@@ -98,13 +105,19 @@ export default {
         })
         .then(res => res.json())
         .then((data) => {
-          status.value = data.status
+          if(data.serverStatus.status) {
+            status.value = data.serverStatus.status
+            serverip.value = "server ip: " + data.ip
+          } else {
+            status.value = data.serverStatus.status
+            serverip.value = ""
+          }
         })
     }
 
     updateStatus()
 
-    return { propsPlayers, status, startServer, stopServer, timestamp, getCommands, changeSettings }
+    return { propsPlayers, status, startServer, stopServer, timestamp, getCommands, changeSettings, serverip }
   }
 }
 </script>
--- src/components/SettingsComponent/index.vue
+++ src/components/SettingsComponent/index.vue
@@ -4,31 +4,80 @@
         <form>
             <div class="settings-container__setting">
                 <label for="world-name">World-name </label>
-                <input type="text" id="world-name" /><br/>
+                <input type="text" id="world-name" v-model="worldname" :placeholder="worldname"/><br/>
             </div>
             <div class="settings-container__setting">
                 <label for="max-player">Max Players </label>
-                <input type="text" id="max-player" /><br/>
+                <input type="text" id="max-player" v-model="maxplayers" :placeholder="maxplayers"/><br/>
+            </div>
+            <div class="settings-container__setting">
+                <label for="max-player">Description </label>
+                <input type="text" id="max-player" v-model="motd" :placeholder="motd"/><br/>
             </div>
             <div class="settings-container__setting">
                 <label for="gamemode">gamemode </label>
-                <select name="gamemode" id="gamemode">
-                    <option value="creative">creative</option>
-                    <option value="survival" selected>survival</option>
-                    <option vlaue="hardcore">hardroce</option>
+                <select name="gamemode" id="gamemode" v-model="gamemode">
+                    <option value="creative" id="creative">creative</option>
+                    <option value="survival" id="survival">survival</option>
+                    <option value="hardcore" id="hardcore">hardcore</option>
                 </select><br/>
             </div>
             <div class="settings-container__setting">
-                <label for="whitelist">whitelist </label>
-                <input type="checkbox" id="whitelist" /><br/>
+                <label for="pvp">pvp </label>
+                <input type="checkbox" id="pvp" v-model="pvp"/><br/>
             </div>
+            <button @click="handleSetting">apply changes</button>
         </form>
     </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 import './index.scss';
 export default {
-    name: "SettingsComponent"
+    name: "SettingsComponent",
+    setup() {
+
+        const worldname = ref("")
+        const maxplayers = ref("")
+        const motd = ref("")
+        const gamemode = ref("")
+        const pvp = ref(false)
+        
+        function getServerSettings() {
+            fetch('http://192.168.1.232:8000/server/settings/get', {
+                method: "get",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(res => res.json())
+            .then((data) => {
+                gamemode.value = data.gamemode
+                pvp.value = data.pvp
+                worldname.value = data.worldname
+                maxplayers.value = data.maxplayers
+                motd.value = data.motd
+            })
+        }
+
+        getServerSettings();
+
+        const handleSetting = () => {
+            if(worldname.value !== "" && maxplayers.value !== "") {
+                fetch("http://192.168.1.232:8000/server/settings", {
+                    method: "Post",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        settings: [gamemode.value, pvp.value, worldname.value, maxplayers.value, motd.value]
+                    }),
+                }).then((res) => res.status === 200)
+            }
+        }
+
+        return { handleSetting, worldname, maxplayers, motd, gamemode, pvp }
+    }
 }
 </script>
